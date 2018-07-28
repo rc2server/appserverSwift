9c8d04373036d6e74ec4f81a087cf999ca72423a
Mark Lilback <mark@lilback.com>
2018-07-28 11:49:23 -0400

    first attempt at launching compute via k8s

--- Sources/Rc2AppServer/K8sServer.swift
+++ Sources/Rc2AppServer/K8sServer.swift
@@ -3,24 +3,78 @@ import MJLLogger
 import BrightFutures
 import PerfectCURL
 import Freddy
+import Stencil
+import PathKit
 
 enum K8sError: Error {
     case connectionFailed
     case invalidResponse
     case impossibleSituation
+    case invalidConfiguration
 }
+
+// stencil templates are in config.k8sStencilPath
 
 class K8sServer {
     private let token: String
+    private let config: AppConfiguration
+    private let stencilEnv: Environment
 
-    init() throws {
+    init(config: AppConfiguration) throws {
+        self.config = config
         // get the api credentials
         do {
             token = try String(contentsOfFile: "/var/run/secrets/kubernetes.io/serviceaccount/token")
+            let fsLoader = FileSystemLoader(paths: [Path(config.k8sStencilPath)])
+            stencilEnv = Environment(loader: fsLoader)
         } catch {
             Log.info("failed to read k8s api credential")
             throw K8sError.connectionFailed
         }
+    }
+
+    /// Fires off a job to kubernetes to start a compute instance for the specified workspace
+    /// - Returns: future is always true if no error happend
+    /// FIXME: need to delay return value until the compute container is running and accepting connections
+    func launchCompute(wspaceId: Int) -> Future<Bool, K8sError> {
+        let promise = Promise<Bool, K8sError>()
+        // for now only support basicComputeJob
+        let jobString: String
+        do {
+            let context = ["wspaceId": String(wspaceId), "computeImage": config.computeImage]
+            jobString = try stencilEnv.renderTemplate(name: "basicComputeJob.json", context: context)
+            // for debugging purposes, log the job request
+            try jobString.write(to: URL(fileURLWithPath: "/tmp/job-\(wspaceId).json"), atomically: true, encoding: .utf8)
+        } catch {
+            Log.error("failed to load compute job template: \(error)")
+            promise.failure(.invalidConfiguration)
+            return promise.future
+        }
+        let rawUrl = "https://kubernetes.default.svc/apis/batch/v1/namespaces/default/jobs"
+        let request = CURLRequest(rawUrl, options: [.sslCAFilePath("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"), .postString(jobString), .httpMethod(.post)])
+        request.addHeader(.authorization, value: "Bearer \(token)")
+        request.addHeader(.accept, value: "application/json")
+        request.addHeader(.contentType, value: "application/json")
+        request.perform { confirmation in 
+            do {
+                let response = try confirmation()
+                let statusCode = response.get(.responseCode)
+                guard statusCode == 201 else {
+                    Log.error("failed to create compute job: \(statusCode ?? 0)")
+                    promise.failure(K8sError.invalidResponse)
+                    return
+                }
+                // HACK: wait a bit to return so have time to start up
+                DispatchQueue.global().asyncAfter(deadline: .now() + .milliseconds(2000)) {
+                    Log.info("delayed return after launch")
+                    promise.success(true)
+                }
+            } catch {
+                Log.error("error in launch job request: \(error)")
+                promise.failure(.invalidResponse)
+            }
+        }
+        return promise.future
     }
 
     /// Looks for a pod running a compute engine for the specified sessionId.
@@ -28,29 +82,93 @@ class K8sServer {
     /// - parameter sessionId: the sessionId to find an open compute session
     /// - returns: the hostname for the specified session, or nil if there is no such session running
     func hostName(wspaceId: Int) -> Future<String?, K8sError> {
-        let rawUrl = "https://kubernetes.default.svc/api/v1/namespaces/default/pods?limit=1&labelSelector=app%3Dappserver"
+        let rawUrl = "https://kubernetes.default.svc/api/v1/namespaces/default/pods?limit=1&labelSelector=workspace%3D\(wspaceId)"
         let request = CURLRequest(rawUrl, options: [.sslCAFilePath("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")])
         request.addHeader(.authorization, value: "Bearer \(token)")
         request.addHeader(.accept, value: "application/json")
 
         let promise = Promise<String?, K8sError>()
-        Log.info("making api request")
         request.perform { confirmation in 
-            Log.info("got api response")
+            let rdata: Data
             do {
                 let response = try confirmation()
-                let rdata = Data(response.bodyBytes)
-                Log.info("got json from server: \(String(data: rdata, encoding: .utf8)!)")
-                let json = try JSON(data: rdata)
-                let ipAddr = try json.getString(at: "items", 0, "status", "podIP")
-                Log.info("got ipAddr \(ipAddr)")
-                promise.success(ipAddr)
+                rdata = Data(response.bodyBytes)
             } catch {
                 Log.error("curl got error: \(error)")
                 promise.failure(K8sError.invalidResponse)
+                return
+            }
+            do {
+                let json = try JSON(data: rdata)
+                let ipAddr = try json.getString(at: "items", 0, "status", "podIP")
+                let phase = try json.getString(at: "items", 0, "phase")
+                Log.info("got ipAddr \(ipAddr), phase=\(phase)")
+                // FIXME: need to return nil if pod isn't running
+                promise.success(ipAddr)
+            } catch {
+                Log.warn("failed to find workspace pod: \(error)")
+                promise.success(nil)
             }
         }
-        Log.info("resumed api network task")
+        return promise.future
+    }
+
+    /// represents the current status of a compute pod
+    struct ComputePodStatus {
+        enum Phase: String { case pending, running, succeeded, failed, unknown }
+        let ipAddr: String
+        let phase: Phase
+
+        var isRunning: Bool { if case .running = phase { return true }; return false }
+    }
+
+
+    /// Looks for a pod running a compute engine for the specified sessionId.
+    ///
+    /// - parameter sessionId: the sessionId to find an open compute session
+    /// - returns: the hostname for the specified session, or nil if there is no such session running
+    func computeStatus(wspaceId: Int) -> Future<ComputePodStatus?, K8sError> {
+        let rawUrl = "https://kubernetes.default.svc/api/v1/namespaces/default/pods?limit=1&labelSelector=workspace%3D\(wspaceId)"
+        let request = CURLRequest(rawUrl, options: [.sslCAFilePath("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")])
+        request.addHeader(.authorization, value: "Bearer \(token)")
+        request.addHeader(.accept, value: "application/json")
+
+        let promise = Promise<ComputePodStatus?, K8sError>()
+        request.perform { confirmation in 
+            let rdata: Data
+            do {
+                let response = try confirmation()
+                rdata = Data(response.bodyBytes)
+                // for debugging puroposes
+                try rdata.write(to: URL(fileURLWithPath: "/tmp/lastStatus.json"))
+            } catch {
+                Log.error("curl got an error: \(error)")
+                promise.failure(K8sError.invalidResponse)
+                return
+            }
+            do {
+                let json = try JSON(data: rdata)
+                let ipAddr = try json.getString(at: "items", 0, "status", "podIP")
+                let phase = try json.getString(at: "items", 0, "status", "phase").lowercased()
+                Log.info("got ipAddr \(ipAddr), phase=\(phase)")
+                let phaseEnum: ComputePodStatus.Phase
+                if let parsedPhase = ComputePodStatus.Phase(rawValue: phase) {
+                    phaseEnum = parsedPhase
+                } else {
+                    phaseEnum = .unknown
+                }
+                let status = ComputePodStatus(ipAddr: ipAddr, phase: phaseEnum)
+                // FIXME: need to return nil if pod isn't running
+                promise.success(status)
+            } catch JSON.Error.indexOutOfBounds(_) {
+                // no pod found
+                Log.info("no pod found for \(wspaceId)")
+                promise.success(nil)
+            } catch {
+                Log.warn("failed to find workspace pod: \(error)")
+                promise.success(nil)
+            }
+        }
         return promise.future
     }
 }
--- Sources/Rc2AppServer/SessionHandler.swift
+++ Sources/Rc2AppServer/SessionHandler.swift
@@ -23,7 +23,7 @@ public class SessionHandler: WebSocketSessionHandler {
 		self.settings = settings
 		if settings.config.computeViaK8s {
 			do {
-				self.k8sServer = try K8sServer()
+				self.k8sServer = try K8sServer(config: settings.config)
 			} catch {
 				Log.error("failed to create K8sServer: \(error)")
 				fatalError("failed to create K8sServer")
@@ -32,12 +32,12 @@ public class SessionHandler: WebSocketSessionHandler {
 		// TODO: need to schedule a timer that cleans up any sessions with no clients
 	}
 	
-	public func handleSession(request req: HTTPRequest, socket: WebSocket)
-	{
+	// validates input from a request for a session
+	private func validateSessionRequest(request req: HTTPRequest, socket: WebSocket) -> (Workspace, User)? {
 		guard let loginToken = req.login else {
 			Log.error("ws attempt without login")
 			reportError(socket: socket, error: SessionError.permissionDenied)
-			return
+			return nil
 		}
 		//figure out the user and the workspace they want to use
 		guard let rawWspaceId = req.urlVariables["wsId"],
@@ -46,7 +46,7 @@ public class SessionHandler: WebSocketSessionHandler {
 			let wspace = rawWspace
 		else {
 			reportError(socket: socket, error: SessionError.invalidRequest)
-			return
+			return nil
 		}
 		guard
 			wspace.userId == loginToken.userId,
@@ -55,53 +55,94 @@ public class SessionHandler: WebSocketSessionHandler {
 		else {
 			Log.warn("user doesn't have permission for requested workspace")
 			reportError(socket: socket, error: SessionError.permissionDenied)
-			return
+			return nil
 		}
+		return (wspace, user)
+	}
+
+	public func handleSession(request req: HTTPRequest, socket: WebSocket)
+	{
+		guard let (wspace, user) = validateSessionRequest(request: req, socket: socket) else {
+			return //already reported error
+		}
+		let wspaceId = wspace.id
 		// now have a workspace & user. find session
 		let computePort = settings.config.computePort
 		lockQueue.sync {
 			var session = activeSessions[wspaceId]
-			if nil == session {
-				session = Session(workspace: wspace, settings: settings)
-				activeSessions[wspaceId] = session
-				getComputeAddress(wspaceId: wspaceId).onSuccess { ipAddr in 
-					do {
-						try session?.startSession(host: ipAddr, port: computePort)
-					} catch {
-						Log.error("startSession failed: \(error)")
-						self.reportError(socket: socket, error: SessionError.failedToConnectToCompute)
-					}
-				}.onFailure { error in 
+			defer {
+				//add connection to session
+				let sessionSocket = SessionSocket(socket: socket, user: user, settings: settings, delegate: session!)
+				session!.add(socket: sessionSocket)
+				sessionSocket.start()
+			}
+			guard nil == session else { return }
+			// need to create session and start compute engine
+			session = Session(workspace: wspace, settings: settings)
+			activeSessions[wspaceId] = session
+			// if using shared compute engine, do that and return
+			guard let server = k8sServer, settings.config.computeViaK8s else {
+				Log.info("returning shared compute host")
+				do {
+					try session?.startSession(host: settings.config.computeHost, port: computePort)
+				} catch {
+					Log.error("error starting session to shared compute engine \(error)")
 					self.reportError(socket: socket, error: SessionError.failedToConnectToCompute)
 				}
+				return
 			}
-			//add connection to session
-			let sessionSocket = SessionSocket(socket: socket, user: user, settings: settings, delegate: session!)
-			session!.add(socket: sessionSocket)
-			sessionSocket.start()
+			// get the ip address of running compute container
+			getComputeIpAddress(wspaceId: wspaceId).flatMap { ipAddr -> Future<String?, K8sError> in
+				// if running, return that ip address 
+				if let curIp = ipAddr {
+					Log.info("found existing ipAddress")
+					return Future<String?, K8sError>(value: curIp)
+				}
+				// launch compute and then get the address of the launched container
+				Log.info("launching compute")
+				return server.launchCompute(wspaceId: wspaceId).flatMap { _ -> Future<String?, K8sError> in 
+					Log.info("launched compute, looking up address")
+					return self.getComputeIpAddress(wspaceId: wspaceId)
+				}
+			}.onSuccess { ipAddr in 
+				// got an existing ip or launched a new one.
+				Log.info("got an existing ipAddr or launched compute and got ip \(ipAddr ?? "nil")")
+				do {
+					guard let ipAddr = ipAddr else { throw K8sError.connectionFailed }
+					try session?.startSession(host: ipAddr, port: computePort)
+				} catch {
+					Log.error("startSession failed: \(error)")
+					self.reportError(socket: socket, error: SessionError.failedToConnectToCompute)
+				}
+			}.onFailure { error in 
+				self.reportError(socket: socket, error: SessionError.failedToConnectToCompute)
+			}
 		}
 	}
 	
-	private func getComputeAddress(wspaceId: Int) -> Future<String, K8sError> {
-		let promise = Promise<String, K8sError>()
-		guard let server = k8sServer, settings.config.computeViaK8s else {
-			Log.info("returning shared compute host")
-			promise.success(settings.config.computeHost)
-			return promise.future
-		}
-		Log.info("looking up ip address")
-		server.hostName(wspaceId: wspaceId).onSuccess { ipAddr in 
-			Log.info("got compute ip \(ipAddr ?? "nil")")
-			promise.success(ipAddr ?? self.settings.config.computeHost)
+	// gets the status info from the k8s server. If there is a pod and it is running, returns the ip address for it
+	private func getComputeIpAddress(wspaceId: Int) -> Future<String?, K8sError> {
+		let promise = Promise<String?, K8sError>()
+		k8sServer!.computeStatus(wspaceId: wspaceId).flatMap { status -> Future<String?, K8sError> in
+			// if not running, then return nil address
+			guard let actualStatus = status else {
+				return Future<String?, K8sError>(value: nil)
+			}
+			// check status
+			if let ipAddr = status?.ipAddr, actualStatus.isRunning {
+				return Future<String?, K8sError>(value: ipAddr)
+			} else {
+				// TODO: need to wait if pending, delete otherwise so it can be launched. return better error
+				Log.info("compute pod exists, but not in running state")
+				return Future<String?, K8sError>(error: .connectionFailed)
+			}
+		}.onSuccess { ipAddr in 
+			promise.success(ipAddr)
 		}.onFailure { error in 
 			promise.failure(error)
 		}
 		return promise.future
 	}
-
-//	private func launchCompute(wspaceId: Int) -> Promise<String> {
-//		return .value("invalid")
-//	}
 
 	/// sends an error message on the socket and then closes it
 	fileprivate func reportError(socket: WebSocket, error: SessionError)
